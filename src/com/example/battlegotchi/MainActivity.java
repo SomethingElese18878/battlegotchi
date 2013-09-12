8846787372afb1b1eca189eaa0e98d9cff980fc1
Norman-Desktop <norman.walker87@gmail.com>
2013-09-12 20:43:41 +0200

    4 top buttons send a msg. btnFeed has another picture when its clicked - next: change state by clicking

--- src/com/example/battlegotchi/MainActivity.java
+++ src/com/example/battlegotchi/MainActivity.java
@@ -4,32 +4,47 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-//	Button btnFeed = (Button) findViewById(R.id.btnFeed);
-//			
-//	btnFeed.setOnClickListener(new View.OnClickListener() {
-//		
-//		@Override
-//		public void onClick(View v) {
-//			// TODO Auto-generated method stub
-//			
-//		}
-//	});
-
+	
+	ImageButton btnFeed;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        addListenerOnButton();
     }
     
-    public void sendMessage(View view) {
-        // Do something in response to button click
-    	System.out.println("Feed pushed");
-
+    public void sendInfo(View view) {
+    	Toast.makeText(MainActivity.this, "Info!", Toast.LENGTH_SHORT).show();
     }
-
+    public void sendFeed(View view) {
+    	Toast.makeText(MainActivity.this, "feed feed!", Toast.LENGTH_SHORT).show();
+    }
+    public void sendMessage(View view) {
+    	Toast.makeText(MainActivity.this, "Send message!", Toast.LENGTH_SHORT).show();
+    }
+    public void sendFight(View view) {
+    	Toast.makeText(MainActivity.this, "Send fight!", Toast.LENGTH_SHORT).show();
+    }
+    
+    //Listener for buttons when they created at runtime    
+//    public void addListenerOnButton() {
+//		
+//    	ImageButton btnInfo = (ImageButton) findViewById(R.id.btnInfo);
+//    	ImageButton btnFeed = (ImageButton) findViewById(R.id.btnFeed);
+//    	
+//		btnFeed.setOnClickListener(new View.OnClickListener() {
+//				
+//			public void onClick(View v) {
+//				Toast.makeText(MainActivity.this, "Feed feed!", Toast.LENGTH_SHORT).show();
+//			}
+//		});
+//			
+//	}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
